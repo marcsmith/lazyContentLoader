e23b95ffa4896150efef214abeec1a74f18fd5a8
Marc Smith <marc.smith@gmail.com>
2011-07-08 16:57:29 -0400

    hopefully from here on out it'll just be bug fixes and enhancements

--- js/mylibs/jquery.lazyContentLoader.js
+++ js/mylibs/jquery.lazyContentLoader.js
@@ -4,26 +4,29 @@
 			var defaults = {
 				activeClass			: 'active',				// css class added to control/content containers when in active state
 				controlItemClass	: 'controls',			// css class of control list items
-				contentItemClass	: 'content',			// css class of content list items
-				usePlaceholder		: true					// put placeholder html in content area while remote content is being fetched
-				//placeholder		: '/images/loading.gif'	// path to placeholder img - this gotta go
-				//timedLoadingInterval
+				contentItemClass	: 'content'				// css class of content list items
 			};
 			var options = $.extend(defaults, options);
 
 
 			return this.each(function() {
-				var target = this;
+				var target = this,			// target object, containing list of controls and list of content (should have same number of <li>'s) - TODO: assert that?
+					loadError = false;		// i'm still not using this for anything...sigh...
 				target.options = options;
 
 				// find & store control and content items as properties (lists) on target
 				_initTargetLists();
 
+				// if content URL is bound to the controls container, then only 1 external call is going to be made (likely, for ALL of the content - or ALL minus the initial panel, etc...that's up to the callback)
+				var controlsContainer = $(target).find('ul.' + options.controlItemClass);
+				if( controlsContainer.data('contentUrl') ) {
+					_setupBindings(controlsContainer);
+				}
+
 				// iterate over control items and perform bindings, etc
 				target.controlItems.each(function(index) {
 					var that = $(this),
-						loadError = false,		// i'm still not using this for anything...sigh...
-						controlIndex = index;	// this is ONLY used when 
+						controlIndex = index;	// this is ONLY used when ... ?
 
 					// set index and target of remote content on the control item
 					that.data('index', index);
@@ -31,61 +34,79 @@
 
 					if(that.data('contentUrl')) {
 						var targetContainer = $(that.data('contentTarget'));
-						if(options.usePlaceholder) {
+						if(options.placeholder) {
 							// this is lame
 							targetContainer.html(options.placeholder);
 						}
-						that.bind('loadContent', function(e, d) {
-							$.ajax({
-								url: that.data('contentUrl'),
-								success: function( data ) {
-									if(options.callback) {
-										// send the index, data & whether or not it was timer-based to the callback
-										options.callback.call( this, that.data('index'), data, (d!=undefined && d.activatedByTrigger) );
-									}
-								},
-								error: function( data ) {
-//									targetContainer.hide().html("");	//TODO: DON'T DO THIS!!!  add error callback
-									loadError = true;
-								}
-							}).then(function() {
-								// once is enough - success or not - so remove ... TODO: maybe once is NOT enough??
-								that.removeData('contentUrl');
-
-								// hacky much?
-								if(d!=undefined && d.activatedByTrigger) {
-									_activateItems(that.data('index'));
-								}
-							});
-						});
+						_setupBindings(that);
 					}
 
 					that.bind(options.triggerAction, function(e) {
-						if(!loadError && that.data('contentUrl')) {
-							$(this).trigger('loadContent', {activatedByTrigger: true}).unbind('loadContent');
-						} else {
+						// TODO: not really using loadError properly...should have error callback and graceful error situation handling (eg., ajax fails, do wha?)
+						if(!loadError) {
+							if(that.data('contentUrl')) {
+								that.trigger('loadContent', {activatedByTrigger: true, idx: that.data('index')}).unbind('loadContent');
+							} else if($(controlsContainer).data('contentUrl')) {
+								$(controlsContainer).trigger('loadContent', {activatedByTrigger: true, idx: that.data('index')}).unbind('loadContent');
+							} else if(options.callback) {
+								// send the index, data & whether or not it was timer-based to the callback
+								options.callback.call( this, that.data('index'), null, true);
+							}
 							_activateItems(that.data('index'));
-							options.callback.call( this, that.data('index'), null, true);	// no data to pass, and wasn't timer based (was action based), so last arg is true
+						} else {
+							// something went wrong?  oh snap, now what?
 						}
 					});
-					// if there's an action to bound our clear to, let's do it
-					if(options.clearAction) {
+				});
+
+				// if there's an action to bound our clear to, let's do it
+				if(options.clearAction) {
+					target.controlItems.each(function(index) {
+						var that = $(this);
 						that.bind(options.clearAction, function(e) {
 							var clearActiveContentItem = target.contentItems[ that.data('index') ];
 							// TODO: would it make sense to clear the control item here, too?
 							$(clearActiveContentItem).removeClass(options.activeClass);
 						});
-					}
-					if(options.timedLoadingInterval) {
-						setTimeout(function() {
-							that.trigger('loadContent').unbind('loadContent');
-						}, (options.timedLoadingInterval*(controlIndex+1)));
-					}
-				});
+					});
+				}
 
+				// why is this *here* ?  why, where else should it be?
 				target.contentItems.each(function(index) {
 					$(this).data('index', index);
 				});
+				
+				/* ************************************************************************************ */
+
+				// setup bindings...
+				function _setupBindings(elem, event, index, wasUserActivated) {
+					elem.bind('loadContent', function(e, d) {
+						var indexClicked = elem.data('index')
+											|| ((d!==undefined && d.idx!==undefined) ? d.idx : -1);	//TODO: -1 ???
+						var wasUserActivated = (d!=undefined && d.activatedByTrigger) ;
+						$.ajax({
+							url: elem.data('contentUrl'),
+							success: function( data ) {
+								if(options.callback) {
+									// send the index, data & whether or not it was timer-based to the callback
+									options.callback.call( this, indexClicked, data, wasUserActivated);
+								}
+							},
+							error: function( data, e ) {
+								//TODO: add error callback
+								loadError = true;
+							}
+						}).then(function() {
+							// once is enough - success or not - so remove ... TODO: maybe once is NOT enough??
+							elem.removeData('contentUrl');
+						});
+					});
+					if(options.timedLoadingInterval) {
+						setTimeout(function() {
+							elem.trigger('loadContent').unbind('loadContent');
+						}, (options.timedLoadingInterval));
+					}
+				}
 
 				// fetch/re-fetch (re-fetch???) our control & content list
 				function _initTargetLists() {
--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
 
 
 /* setup external content URLs */
+/* pushed these into the demo files....heck, they're just demos...
 (function() {
 	$("#nav li:nth-child(1)").data('contentUrl', './ext/nav1.html');
 	$("#nav li:nth-child(2)").data('contentUrl', './ext/nav2.html');
@@ -12,14 +13,15 @@
 	$("#nav li:nth-child(5)").data('contentUrl', './ext/nav5.html');
 	$("#nav li:nth-child(6)").data('contentUrl', './ext/nav6.html');
 
-	/* this first one is loaded onpage load...so no need to bind this here */ 
+	// this first one is loaded onpage load...so no need to bind this here
 //	$("#tabbedContent li:nth-child(1)").data('contentUrl', './ext/tab1.html');
 	$("#tabbedContent li:nth-child(2)").data('contentUrl', './ext/tab2.html');
 
-	/* this first one is loaded onpage load...so no need to bind this here */ 
+	// this first one is loaded onpage load...so no need to bind this here
 //	$("#carousel li:nth-child(1)").data('contentUrl', './ext/caro1.html');
 	$("#carousel li:nth-child(2)").data('contentUrl', './ext/caro2.html');
 	$("#carousel li:nth-child(3)").data('contentUrl', './ext/caro3.html');
+	$("#carousel li:nth-child(4)").data('contentUrl', './ext/caro4.html');
 
 })();
-
+*/
